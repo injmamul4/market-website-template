f9c775f3ae7c4b15892b0a2fcc6d33248a622307
injmamul4 <injmamul1365@gmail.com>
2025-04-03 16:27:14 +0530

    Add files via upload

---
+++ envato market/script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const questions = document.querySelectorAll(".faq-question");
+
+//     questions.forEach(question => {
+//         question.addEventListener("click", function() {
+//             const answer = this.nextElementSibling;
+//             const toggleBtn = this.querySelector(".toggle-btn");
+
+//             if (answer.style.display === "block") {
+//                 answer.style.display = "none";
+//                 toggleBtn.textContent = "+";
+//             } else {
+//                 answer.style.display = "block";
+//                 toggleBtn.textContent = "−";
+//             }
+//         });
+//     });
+// });
